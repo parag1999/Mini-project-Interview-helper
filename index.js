1925f88163585e90bb75cff1a6c856ed4b47725a
parag1999 <paragvaid3496@gmail.com>
2019-10-06 16:03:23 +0530

    Done Setup

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var client = require('./knexFile')
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
+
+const getTest =  async() => {
+    var test = await client.raw('select * from users').then((result)=> result[0])
+    return test
+}
+
+const testFunc = async() => {
+    var finalTest = await getTest()
+    return finalTest.map((val)=> (val.user_id))
+}
+
+
+app.get('/signup',async (req, res) => {
+    let testVal = await testFunc()
+    console.log(testVal)
+   res.render('signup',{data:testVal}) 
+});
+
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
---
+++ knexFile.js
@@ -0,0 +1,9 @@
+module.exports = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : 'localhost',
+      user : 'root',
+      password : 'Parag@1999',
+      database : 'dbms_v1'
+    }
+  });
