8019503b2a0cf5ffaa4826f56ef3109fe60216f2
Palash Rathod <rathodpalash3@gmail.com>
2019-10-06 16:44:26 +0530

    Sign Up  Page Done

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var app = express()
 var bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", function (req, res) {
+    res.render("signup");
+})
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
